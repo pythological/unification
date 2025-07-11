a884f34a741f6ed53f88cec82054ba87fe10f5df
Ryan Whitmore <whitmore5@llnl.gov>
2025-07-11 16:13:28 -0700

    Migrate to setuptools-scm from versioneer for dynamic package versioning.

--- unification/__init__.py
+++ unification/__init__.py
@@ -1,7 +1,6 @@
-from ._version import get_versions
+from importlib.metadata import version
 from .core import assoc, reify, unify
 from .more import unifiable
 from .variable import Var, isvar, var, variables, vars
 
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = version("logical-unification")
